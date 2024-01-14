55d46fe823dc408ea747fe10bda0fba09acc8dcf
Michał Wojciechowski <kontogry96@gmail.com>
2024-01-14 14:38:43 +0100

    Fix moving tasks

--- backend/Kanban.Dashboard.Core/Features/Tasks/Commands/MoveTaskCommand.cs
+++ backend/Kanban.Dashboard.Core/Features/Tasks/Commands/MoveTaskCommand.cs
@@ -28,8 +28,6 @@ namespace Kanban.Dashboard.Core.Features.Tasks.Commands
         public async Task<bool> Handle(MoveTaskCommand request, CancellationToken cancellationToken)
         {
             var switchColumnResult = await _mediator.Send(new SwitchTaskColumnCommand() { ColumnTargetId = request.ColumnTargetId, TaskId = request.TaskId }, cancellationToken);
-            if (switchColumnResult == false)
-                throw new Exception("Switching column failed.");
 
             var column = await _context.Columns.FirstOrDefaultAsync(x => x.Id == request.ColumnTargetId, cancellationToken);
             if (column == null)
@@ -39,22 +37,66 @@ namespace Kanban.Dashboard.Core.Features.Tasks.Commands
             var task = await tasks.FirstOrDefaultAsync(x => x.Id == request.TaskId, cancellationToken: cancellationToken);
             if (task == null)
                 throw new Exception("KanbanTask not found.");
-
+           
             var order = Math.Max(request.Order, 0);
-            var tasksWithOrderGreaterOrEqual = await tasks.Where(x => x.Order >= order).ToListAsync(cancellationToken);
-            foreach (var taskWithOrderGreaterOrEqual in tasksWithOrderGreaterOrEqual)
+
+            if (switchColumnResult.isSuccess)
+            {
+                foreach (var taskWithOrderGreaterOrEqual in tasks.Where(x => x.Order <= order - 1 && x.Id != task.Id && task.Parents.Any() == false))
+                {
+                    taskWithOrderGreaterOrEqual.Order--;
+                }
+
+                foreach (var taskWithOrderGreaterOrEqual in tasks.Where(x => x.Order >= order && x.Id != task.Id && task.Parents.Any() == false))
+                {
+                    taskWithOrderGreaterOrEqual.Order++;
+                }
+
+                task.Order = order;
+
+                var i = 1;
+                foreach (var t in (await tasks.Where(x => x.Parents.Any() == false).ToListAsync(cancellationToken)).OrderBy(x => x.Order))
+                {
+                    t.Order = i++;
+                }
+                await tasks.Where(x => x.Parents.Any() == true).ForEachAsync(x => x.Order = 0, cancellationToken);
+
+                var oldColumn = _context.KanbanTasks.Where(x => x.ColumnId == switchColumnResult.oldColumn);
+                i = 1;
+                foreach (var t in (await oldColumn.Where(x => x.Parents.Any() == false).ToListAsync(cancellationToken)).OrderBy(x => x.Order))
+                {
+                    t.Order = i++;
+                }
+                return await _context.SaveChangesAsync(cancellationToken) > 0;
+            }
+
+            var tasksWithOrderLessOrEqual = order > (switchColumnResult.isSuccess ? 0 : task.Order)
+                ? tasks.Where(x => x.Order <= order && x.Id != task.Id && task.Parents.Any() == false)
+                : tasks.Where(x => x.Order < order - 1 && x.Id != task.Id && task.Parents.Any() == false);
+
+            foreach (var taskWithOrderGreaterOrEqual in tasksWithOrderLessOrEqual)
+            {
+                taskWithOrderGreaterOrEqual.Order--;
+            }
+
+            var tasksWithOrderGreater = order > (switchColumnResult.isSuccess ? 0 : task.Order)
+                ? await tasks.Where(x => x.Order > order && x.Id != task.Id && task.Parents.Any() == false).ToListAsync(cancellationToken)
+                : await tasks.Where(x => x.Order >= order && x.Id != task.Id && task.Parents.Any() == false).ToListAsync(cancellationToken);
+
+            foreach (var taskWithOrderGreaterOrEqual in tasksWithOrderGreater)
             {
                 taskWithOrderGreaterOrEqual.Order++;
             }
             task.Order = order;
 
-            var allTasksInColumn = (await tasks.ToListAsync(cancellationToken)).OrderBy(x => x.Order);
+            var allTasksInColumn = (await tasks.Where(x => x.Parents.Any() == false).ToListAsync(cancellationToken)).OrderBy(x => x.Order);
             var normalizedOrder = 1;
             foreach (var t in allTasksInColumn)
             {
                 t.Order = normalizedOrder++;
             }
 
+            await tasks.Where(x => x.Parents.Any() == true).ForEachAsync(x => x.Order = 0, cancellationToken);
             return await _context.SaveChangesAsync(cancellationToken) > 0;
         }
     }
--- backend/Kanban.Dashboard.Core/Features/Tasks/Commands/SwitchTaskColumnCommand.cs
+++ backend/Kanban.Dashboard.Core/Features/Tasks/Commands/SwitchTaskColumnCommand.cs
@@ -6,13 +6,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Kanban.Dashboard.Core.Features.Tasks.Commands
 {
-    public class SwitchTaskColumnCommand : IRequest<bool>
+    public class SwitchTaskColumnCommand : IRequest<(bool isSuccess, Guid oldColumn)>
     {
         public Guid TaskId { get; set; }
         public Guid ColumnTargetId { get; set; }
     }
 
-    public class SwitchTaskColumnCommandHandler : IRequestHandler<SwitchTaskColumnCommand, bool>
+    public class SwitchTaskColumnCommandHandler : IRequestHandler<SwitchTaskColumnCommand, (bool isSuccess, Guid oldColumn)>
     {
         private readonly IApplicationDbContext _context;
 
@@ -21,22 +21,29 @@ namespace Kanban.Dashboard.Core.Features.Tasks.Commands
             _context = context;
         }
 
-        public async Task<bool> Handle(SwitchTaskColumnCommand request, CancellationToken cancellationToken)
+        public async Task<(bool isSuccess, Guid oldColumn)> Handle(SwitchTaskColumnCommand request, CancellationToken cancellationToken)
         {
-            var task = await _context.KanbanTasks.Include(x=>x.Column).FirstOrDefaultAsync(x=>x.Id == request.TaskId, cancellationToken: cancellationToken);
+            var task = await _context.KanbanTasks.Include(x => x.Column).FirstOrDefaultAsync(x => x.Id == request.TaskId, cancellationToken: cancellationToken);
             if (task == null)
                 throw new Exception("KanbanTask not found.");
 
-            var column = await _context.Columns.Include(x => x.Board).FirstOrDefaultAsync(x=>x.Id == request.ColumnTargetId, cancellationToken);
+            var oldColumnId = task.ColumnId;
+
+            var column = await _context.Columns.Include(x => x.Board).FirstOrDefaultAsync(x => x.Id == request.ColumnTargetId, cancellationToken);
             if (column == null || task.Column.BoardId != column?.BoardId)
                 throw new Exception("Target column is located in different board than source column or target column does not exist.");
+
+            if (request.ColumnTargetId == task.ColumnId)
+                return (false, Guid.Empty);
 
             task.DateOfModification = DateTime.UtcNow;
             task.Column.DateOfModification = DateTime.UtcNow;
             column.DateOfModification = DateTime.UtcNow;
             column.Board.DateOfModification = DateTime.UtcNow;
             task.ColumnId = request.ColumnTargetId;
-            return await _context.SaveChangesAsync(cancellationToken) > 0;
+            var result = await _context.SaveChangesAsync(cancellationToken) > 0;
+
+            return (result, oldColumnId);
         }
     }
 }
